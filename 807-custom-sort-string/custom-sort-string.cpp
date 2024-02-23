159610fe9a89fb3dd2f0037e378fa7f446c59876
Aryan Mishra <aryan29mishra@gmail.com>
2024-02-23 14:30:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (38.37%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        vector<pair<char, int>> v;
-        auto compare = [](pair<char, int> a, pair<char, int> b) {
-            return a.second < b.second;
+        //vector<pair<char, int>> v;
+        // auto compare = [&](pair<char, int> a, pair<char, int> b) {
+        //     return a.second < b.second;
+        // };
+        // for(int i=0; i < s.length(); i++){
+        //     pair<char, int> t;
+        //     if(order.find(s[i]) == string::npos){
+        //         v.push_back(make_pair(s[i], -1));   
+        //     }
+        //     else {
+        //         v.push_back(make_pair(s[i], order.find(s[i])));
+        //     }
+        // }
+        // sort(v.begin(), v.end(), compare);
+        // string ans="";
+        // for(int i=0; i<v.size(); i++){
+        //     ans += v[i].first;
+        // }
+        // return ans;
+        auto compare = [&] (char a, char b){
+            return order.find(a) < order.find(b);
         };
-        for(int i=0; i < s.length(); i++){
-            pair<char, int> t;
 
-            if(order.find(s[i]) == string::npos){
-                v.push_back(make_pair(s[i], -1));
-    
-            }
-            else {
-                v.push_back(make_pair(s[i], order.find(s[i])));
-
-            }
-        }
-        sort(v.begin(), v.end(), compare);
-        string ans="";
-        for(int i=0; i<v.size(); i++){
-            ans += v[i].first;
-        }
-        return ans;
-        
+        sort(s.begin(), s.end(), compare);
+        return s;
     }
 
 };
