800d6f4d8ba7db2189a27abcf61613c3a44619c9
Aryan Mishra <aryan29mishra@gmail.com>
2024-04-18 22:36:47 +0530

    Time: 4 ms (99.58%) | Memory: 46.1 MB (14.46%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -6,37 +6,35 @@ class Solution {
                 if((i==0 && i==grid.length -1)) p+=2*grid[i][j];
                 else if(i==0){
                     p+=grid[i][j];
-                    p+=rev(grid[i+1][j]) & grid[i][j];
+                    p+=~(grid[i+1][j]) & grid[i][j];
                 }
                 else if(i==grid.length-1){
                     p+=grid[i][j];
-                    p+=rev(grid[i-1][j]) & grid[i][j];
+                    p+=~(grid[i-1][j]) & grid[i][j];
                 }
                 else{
-                    p+=rev(grid[i-1][j]) & grid[i][j];
-                    p+=rev(grid[i+1][j]) & grid[i][j];
+                    p+=~(grid[i-1][j]) & grid[i][j];
+                    p+=~(grid[i+1][j]) & grid[i][j];
                 }
                 if(j==0 && j==grid[i].length-1){
                     p+=2*grid[i][j];
                 }
                 else if(j==0){
                     p+=grid[i][j];
-                    p+=rev(grid[i][j+1]) & grid[i][j];
+                    p+=~(grid[i][j+1]) & grid[i][j];
                 } 
                 else if(j==grid[i].length-1){
                     p+=grid[i][j];
-                    p+=rev(grid[i][j-1]) & grid[i][j];
+                    p+=~(grid[i][j-1]) & grid[i][j];
                 }
                 else {
-                    p+=rev(grid[i][j-1]) & grid[i][j];
-                    p+=rev(grid[i][j+1]) & grid[i][j];
+                    p+=~(grid[i][j-1]) & grid[i][j];
+                    p+=~(grid[i][j+1]) & grid[i][j];
                 }
                 
             }
         }
         return p;
     }
-    private int rev(int a){
-        return (a==0) ? 1 : 0;
-    }
+    
 }
